d7c77ac629753cda83f4f99f8955c6e4b45739c6
Ericsen Tsai <bommer95175@gmail.com>
2023-07-22 00:45:26 +0800

    feat: add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,60 @@
-console.log(process.env.PORT)
+import express from 'express'
+
+const app = express()
+const router = express.Router()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Internal Server Error')
+})
+
+type IGResponse = {
+  graphql: {
+    shortcode_media: {
+      display_resources: {
+        src: string
+      }[]
+    }
+  }
+}
+
+const getIGPostImgSRCs = async ({ postUrl }: { postUrl: string }) => {
+  const url = new URL(postUrl)
+  const res = await fetch(
+    url.origin + url.pathname + '?' + process.env.SECRET_QUERY_STRING,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+  const data = (await res.json()) as IGResponse
+  return {
+    urls:
+      data?.graphql?.shortcode_media.display_resources.map(
+        (resource) => resource.src
+      ) || [],
+    raw: data,
+  }
+}
+
+// Handle GET requests to the root URL
+router.get('/post', async (req, res) => {
+  const postUrl = req.query.post_url as string
+  if (!postUrl) {
+    res.status(400).send('No URL specified')
+  }
+  const imgUrls = await getIGPostImgSRCs({ postUrl })
+  res.send({ imgUrls })
+})
+
+app.use(router)
+
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`)
+})
